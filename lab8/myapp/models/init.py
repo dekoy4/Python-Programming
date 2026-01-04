bd02fccf62662f55879f5fef53a4eea424eb5305
dekoy4 <yulyakshin4@gmail.com>
2026-01-04 23:42:16 +0500

    Create init.py

---
+++ lab8/myapp/models/init.py
@@ -0,0 +1,9 @@
+"""Пакет с моделями предметной области."""
+
+from .author import Author
+from .app import App
+from .user import User
+from .currency import Currency
+from .user_currency import UserCurrency
+
+__all__ = ['Author', 'App', 'User', 'Currency', 'UserCurrency']
