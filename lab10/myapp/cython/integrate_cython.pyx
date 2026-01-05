3c7d065e42281f91a9872f39c736145feb090c35
dekoy4 <yulyakshin4@gmail.com>
2026-01-05 19:12:40 +0500

    Update integrate_cython.pyx

--- lab10/myapp/cython/integrate_cython.pyx
+++ lab10/myapp/cython/integrate_cython.pyx
@@ -1,20 +1,12 @@
-#cython: language_level=3, boundscheck=False, wraparound=False
+#cython: language_level=3
 
 cdef extern from "math.h":
-    double cos(double)
+    double cos(double x)
 
-def integrate_cos_cython(double a, double b, int n_iter):
-    cdef double acc = 0.0
-    cdef double step = (b - a) / n_iter
+def integrate_cos(double a, double b, int n):
+    cdef double sum = 0.0
+    cdef double h = (b - a) / n
     cdef int i
-    for i in range(n_iter):
-        acc += cos(a + i * step) * step
-    return acc
-
-def integrate_python(f, double a, double b, int n_iter):
-    cdef double acc = 0.0
-    cdef double step = (b - a) / n_iter
-    cdef int i
-    for i in range(n_iter):
-        acc += f(a + i * step) * step
-    return acc
+    for i in range(n):
+        sum += cos(a + i * h) * h
+    return sum
