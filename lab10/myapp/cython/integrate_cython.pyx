37c85af8a092d2adb5e4d55de73be56f038a34b5
dekoy4 <yulyakshin4@gmail.com>
2026-01-05 19:10:06 +0500

    Update integrate_cython.pyx

--- lab10/myapp/cython/integrate_cython.pyx
+++ lab10/myapp/cython/integrate_cython.pyx
@@ -1,28 +1,20 @@
+#cython: language_level=3, boundscheck=False, wraparound=False
+
 cdef extern from "math.h":
-    double cos(double x)
+    double cos(double)
 
 def integrate_cos_cython(double a, double b, int n_iter):
-    """
-    Cython оптимизация для cos(x).
-    """
     cdef double acc = 0.0
     cdef double step = (b - a) / n_iter
     cdef int i
-    
     for i in range(n_iter):
         acc += cos(a + i * step) * step
-    
     return acc
 
 def integrate_python(f, double a, double b, int n_iter):
-    """
-    Универсальная версия для Python функций.
-    """
     cdef double acc = 0.0
     cdef double step = (b - a) / n_iter
     cdef int i
-    
     for i in range(n_iter):
         acc += f(a + i * step) * step
-    
     return acc
