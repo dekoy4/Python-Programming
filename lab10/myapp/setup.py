9a18b9038351882a1c1d9ef366f4fc80351643a8
dekoy4 <yulyakshin4@gmail.com>
2026-01-05 18:44:41 +0500

    Update setup.py

--- lab10/myapp/setup.py
+++ lab10/myapp/setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 from Cython.Build import cythonize
-import numpy as np
+from Cython.Distutils import build_ext
+import numpy
 
 setup(
-    ext_modules=cythonize("integrate_cython.pyx", annotate=True),
-    include_dirs=[np.get_include()]
+    name="lab10-integration",
+    ext_modules=cythonize(
+        ["cython/integrate_cython.pyx"],
+        compiler_directives={'language_level': "3"},
+        annotate=True  # Создает HTML отчет
+    ),
+    include_dirs=[numpy.get_include()],
+    zip_safe=False,
 )
